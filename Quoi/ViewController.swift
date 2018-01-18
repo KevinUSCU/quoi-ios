22581abd1c6e2a58e9669bc651c2160f56632855
Kevin Springer <KevinUSCU@outlook.com>
2018-01-18 11:33:07 -0800

    Initial Commit

---
+++ Quoi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quoi
+//
+//  Created by Kevin Springer on 1/18/18.
+//  Copyright © 2018 Kevin Springer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
